a48ded4cac133f0a2e1f5033e13899c9ea019776
schouhan20 <109410371+schouhan20@users.noreply.github.com>
2022-09-13 21:56:00 +0530

    calling the sv in console

--- translate.js
+++ translate.js
@@ -1,11 +1,21 @@
 var btnTranslate = document.querySelector("#btn-translate")
 var txtInput = document.querySelector("#txt-input")
 var outputDiv = document.querySelector("#output")
+ 
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
-
+function getTranslationURL(text ){
+    return serverURL + "?" + "text" + text
+}
 
 function clickHandler() {
-   outputDiv.innerText = txtInput.value
+   outputDiv.innerText = txtInput.value;
+   var inputText = txtInput.value;
+    // calling the sv for processing
+    fetch(getTrabsationURL(inputText))
+    .then(response => response.json())
+    .then(json => console.log(json.contents.translated))
 };
 
-btnTranslate.addEventListener("click", clickHandler)
+btnTranslate.addEventListener("click", clickHandler) {
+}
