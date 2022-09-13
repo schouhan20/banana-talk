9be9c622839f979095f9c18769bb3645818ca49a
schouhan20 <109410371+schouhan20@users.noreply.github.com>
2022-09-13 19:10:19 +0530

    input and console

---
+++ translate.js
@@ -0,0 +1,11 @@
+var btnTranslate = document.querySelector("#btn-translate")
+var txtInput = document.querySelector("#txt-input")
+
+
+
+function clickHandle () {
+    console.log("Clicke!"
+    console.log("input", txtInput.value)
+};
+
+btnTranslate.addEventListener("click", clickHandle)
